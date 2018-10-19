fbebd8541e3e409a602a3cff97d33da2f56d1297
李杭恩 <lihangen@lihangendeMacBook-Pro.local>
2018-10-19 21:47:00 +0800

    add search and css

--- src/Map.js
+++ src/Map.js
@@ -5,15 +5,19 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 class MapCreate extends Component{
     render(){
         return(
-            <div id='map'>
-                <Map google={this.props.google}
-                     initialCenter={{lat:24.479833,lng:118.089425}}
-                     zoom={14}>
-                    <Marker
-                        title={'russell'}
-                        position={{lat:24.489111,lng:118.108976}}
-                    />
-                </Map>
+            <div>
+                <input className='search-contacts' type="text"
+                />
+                <div id='map'>
+                    <Map google={this.props.google}
+                      initialCenter={{lat:24.479833,lng:118.089425}}
+                      zoom={14}>
+                     <Marker
+                         title={'russell'}
+                         position={{lat:24.489111,lng:118.108976}}
+                      />
+                     </Map>
+                </div>
             </div>
         )
     }
