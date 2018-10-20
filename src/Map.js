607918e32f2037beb09088c2c4e5d98c3ddb47f8
Russell <lihangen@lihangendeMacBook-Pro.local>
2018-10-20 19:00:16 +0800

    init

---
+++ src/Map.js
@@ -0,0 +1,10 @@
+import React ,{Component}from 'react'
+import ReactDom from 'react-dom'
+
+class CreateMap extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
