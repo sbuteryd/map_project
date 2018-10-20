6e0e156e101ebe3e2b98356c10e5747c119bac47
Russell <lihangen@lihangendeMacBook-Pro.local>
2018-10-20 13:58:05 +0800

    add five marker

--- src/Map.js
+++ src/Map.js
@@ -22,10 +22,27 @@ class MapCreate extends Component{
                       initialCenter={{lat:24.479833,lng:118.089425}}
                       zoom={14}>
                      <Marker
-                         title={'russell'}
-                         position={{lat:24.489111,lng:118.108976}}
-                      />
+                        title={'厦门图书馆'}
+                        position={{lat:24.489111,lng:118.108976}}
+                    />
+                        <Marker
+                            title={'厦门博物馆'}
+                            position={{lat:24.490944,lng:118.109159}}
+                        />
+                        <Marker
+                            title={'厦门科技馆'}
+                            position={{lat:24.490618,lng:118.108244}}
+                        />
+                        <Marker
+                            title={'厦门市体育中心'}
+                            position={{lat:24.486374,lng:118.1085636}}
+                        />
+                        <Marker
+                            title={'厦门工人体育馆'}
+                            position={{lat:24.489698,lng:118.111495}}
+                        />
                      </Map>
+                    marker ={}
                 </div>
             </div>
         )
